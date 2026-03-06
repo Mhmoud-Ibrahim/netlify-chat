6b4780077949fe5772972cf77a9bd237e88c32ed
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-03-07 03:31:05 +0400

    ddd

--- src/components/SocketContext.tsx
+++ src/components/SocketContext.tsx
@@ -223,7 +223,8 @@ useEffect(() => {
            newSocket.on("receive_group_msg", (data: MsgData) => {
             setMessages((prev) => [...prev, data]);
             if (data.senderId !== userId) {
-                notifySound.play().catch(() => {});
+                 const sound = new Audio('/notification.mp3'); 
+        sound.play().catch(() => console.log("Audio play deferred"));
             }
         });
 
