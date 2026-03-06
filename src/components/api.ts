9ba306d248d82c7022b7b383c97ddf09793692ec
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-03-06 22:16:12 +0400

    dd

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://m2dd-serverchatapp.hf.space",
+    baseURL:"https://m2dd-serverchatapp.hf.space",
     withCredentials: true 
 });
 
