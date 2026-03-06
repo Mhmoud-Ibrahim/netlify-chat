0dfa77e60af213f6c97e4098549a46197401ec6c
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-03-07 03:34:51 +0400

    axios

--- src/components/api.ts
+++ src/components/api.ts
@@ -2,23 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL:"https://m2dd-serverchatapp.hf.space",
-    
     withCredentials: true 
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            // الأفضل التأكد من وجود كائن headers لتجنب الأخطاء
-            config.headers = config.headers || {};
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+
 
 export default api;
